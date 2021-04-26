905496bfabef014f2e573a78219697685228278a
Alexandre LEYMARIE <alexandre.j.leymarie@gmail.com>
2021-04-26 16:27:18 +0200

    Fixed sounds

--- sounds.js
+++ sounds.js
@@ -1,3 +1,3 @@
-let collectSound = AudioFX('sounds/collect', { formats: ['mp3'], pool: 12 });
-let landSound = AudioFX('sounds/land', { formats: ['mp3'], pool: 1 });
-let jumpSound = AudioFX('sounds/jump', { formats: ['mp3', 'ogg', 'wav'], pool: 1 });
+let collectSound = AudioFX('Sounds/collect', { formats: ['mp3'], pool: 12 });
+let landSound = AudioFX('Sounds/land', { formats: ['mp3'], pool: 1 });
+let jumpSound = AudioFX('Sounds/jump', { formats: ['mp3', 'ogg', 'wav'], pool: 1 });
