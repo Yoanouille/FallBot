e86d68301ee0c2b0933c5b7e2f2a940332d87e84
Alexandre LEYMARIE <alexandre.j.leymarie@gmail.com>
2021-04-26 15:01:22 +0200

    Exported one sound to .wav

--- sounds.js
+++ sounds.js
@@ -1,3 +1,3 @@
 let collectSound = AudioFX('sounds/collect', { formats: ['mp3'], pool: 12 });
 let landSound = AudioFX('sounds/land', { formats: ['mp3'], pool: 1 });
-let jumpSound = AudioFX('sounds/jump', { formats: ['mp3', 'ogg'], pool: 1 });
+let jumpSound = AudioFX('sounds/jump', { formats: ['mp3', 'ogg', 'wav'], pool: 1 });
